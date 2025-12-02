5bea9d4943256ff4568a5c77114b50d48dbc8acd
Jay Vee Dignadice <jvdignadice@gmail.com>
2025-12-02 17:20:47 +0800

    added test folder

---
+++ test/test.api.project.one/Infrastructure/ServiceClassTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.api.project.one.Infrastructure
+{
+    internal class ServiceClassTest
+    {
+    }
+}
---
+++ test/test.api.project.one/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace test.api.project.one
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
